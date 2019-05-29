fa39d6a48099d37cef85371c883b03e71990cea5
Aaron <19629983+Aaron308@users.noreply.github.com>
2019-05-30 09:54:29 +1000

    Update periodicity_ribo.sh

--- periodicity_ribo.sh
+++ periodicity_ribo.sh
@@ -19,7 +19,7 @@ mkdir periodicity
 while read i;
 do
 echo $alignFolder/$i/${i}.bam
-samtools view -h $alignFolder/$i/${i}.bam | awk '(length($10) > 27 && length($10) < 29) || $1 ~ /^@/' | samtools view -bS - > $alignFolder/$i/${i}_28.bam
+samtools view -h -q 10 $alignFolder/$i/${i}.bam | awk '(length($10) > 27 && length($10) < 29) || $1 ~ /^@/' | samtools view -bS - > $alignFolder/$i/${i}_28.bam
 mkdir periodicity/$i
 intersectBed -bed -wo -S -abam $alignFolder/$i/${i}_28.bam -b $reference > periodicity/$i/${i}_intersect.temp.bed
 cut -f 1-3,6,14-16,19 periodicity/$i/${i}_intersect.temp.bed > periodicity/$i/${i}_intersect.bed
